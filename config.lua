ddb18944bf61a15312cc107d8b4bf5a63733defe
Baspel <77331512+BaspelOp@users.noreply.github.com>
2022-12-18 11:02:03 +0100

    added config to playlist and target
    new option to turn on ox_target (optional)
    new config to Playlist, you can make playlist from static URL

--- config.lua
+++ config.lua
@@ -1,13 +1,18 @@
 Config = {}
 
+--- Common settings ---
 Config.Debug = true -- If you want debug in console
 Config.DefaultVolume = 0.1 -- Accepted values are 0.01 - 1
 Config.Distance = 5.0 -- Dont touch this
 
+--- Target system ---
+Config.ox_target = true -- If you want to use qtarget you need also polyzone script
+
+--- Locations ---
 Config.Locations = {
     {
         name = 'Vanilla', -- Name of zone
-        coords = vec3(117.63, -1283.91, 28.27), -- Coordinates where menu will appear if you are nearby
+        coords = vec3(120.5638, -1280.9021, 29.4805), -- Coordinates where menu will appear if you are nearby
         radius = 30, -- Playing music distance (radius)
         distance = 2.5, -- Menu appear distance
         isPlaying = false -- Dont touch this!!!!
@@ -65,5 +70,37 @@ Config.Language = {
     ['songSel'] = 'Song Selection',
     ['url'] = 'Youtube URL',
     ['musicVolume'] = 'Music Volume',
-    ['musicVolumeNm'] = 'Min: 0.01 - Max: 1' -- Pls dont change numbers (0.01 - 1)
+    ['musicVolumeNm'] = 'Min: 0.01 - Max: 1', -- Pls dont change numbers (0.01 - 1)
+
+    --- Playlist ---
+    ['playlistMenu'] = '🎶 | DJ Pult Playlist',
+    ['playlistDesc'] = 'Play a song from playlist',
+    ['playlistMenuTitle'] = '🎶 | Play a song'
+}
+
+Config.Playlist = {
+    --- First Song
+    ['first'] = '💿 | Mess', -- Name of first song
+    ['desc_first'] = 'Description of the song', -- Description of the song
+    ['music_first_id'] = 'https://www.youtube.com/watch?v=-Kjrf-pxQc4', -- Url from YT
+
+    --- Second Song ---
+    ['second'] = '💿 | Shiver', -- Name of second song
+    ['desc_second'] = 'Description of the song',
+    ['music_second_id'] = 'https://www.youtube.com/watch?v=NdUNtHqY5r8',
+
+    --- Third Song ---
+    ['third'] = '💿 | Good With It', -- Name of third song
+    ['desc_third'] = 'Description of the song',
+    ['music_third_id'] = 'https://www.youtube.com/watch?v=RInypZYiiDM',
+
+    --- Fourth Song ---
+    ['fourth'] = '💿 | Back To You',
+    ['desc_fourth'] = 'Description of the song',
+    ['music_fourth_id'] = 'https://www.youtube.com/watch?v=rrzHAoA-oRI',
+
+    --- Fifth Song ---
+    ['fifth'] = '💿 | Curse',
+    ['desc_fifth'] = 'Description of the song',
+    ['music_fifth_id'] = 'https://www.youtube.com/watch?v=XsmuiDRKbDk'
 }
